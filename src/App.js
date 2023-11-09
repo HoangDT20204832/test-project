555ea75f6ef1ed9d3a288878ecd583a18da57207
Tien Hoang <duongtienhoang2002@gmail.com>
2023-11-09 14:16:33 +0700

    feat: add component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routers } from "./routes";
+import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          {routers.map((route) => {
+            //những trang nào có isShowHeader = true thì sẽ có HeaderComponent
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment;
+            return (
+              <Route
+                key={route.path}
+                //path: chỉ đường dẫn hướng tới trang của element={<Page/>}
+                path={route.path}
+                // element để chỉ trang hiển thị
+                element={
+                  <Layout>
+                    <route.page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </Router>
     </div>
   );
 }
